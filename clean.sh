14bbf43ec5da1dbc002bdef385c2b74d9d5e8f5b
Jeroen Noels <jeroen.noels@telenet.be>
2020-12-06 15:39:19 +0100

    clean.sh

--- clean.sh
+++ clean.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-find /home/papa/john-baez-challenge/ -name "*.o"  -print0 | xargs -r -0 /bin/rm
-find /home/papa/john-baez-challenge/ -name "*.hi" -print0 | xargs -r -0 /bin/rm
-find /home/papa/john-baez-challenge/ -name "*~"   -print0 | xargs -r -0 /bin/rm
-/bin/rm /home/papa/john-baez-challenge/test/Main
+find ../john-baez-challenge/ -name "*.o"  -print0 | xargs -r -0 /bin/rm
+find ../john-baez-challenge/ -name "*.hi" -print0 | xargs -r -0 /bin/rm
+find ../john-baez-challenge/ -name "*~"   -print0 | xargs -r -0 /bin/rm
+# remove the executable
+/bin/rm -f ../john-baez-challenge/test/Main
